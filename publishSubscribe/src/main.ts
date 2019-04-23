5b68fb5e9db1634c39366cec87211ad453a16d4f
darksystem <diogo.rey@hotmail.com>
2019-04-23 22:06:37 +0100

    Import Project

---
+++ publishSubscribe/src/main.ts
@@ -0,0 +1,28 @@
+import { Queue } from './queue'
+
+
+let queue = new Queue.BoundedQueue<string>(1);
+// let queue = new UnboundedQueue<string>();
+
+// 'some value'
+let cenas = queue.pop();
+console.log(cenas);
+let tretas: any;
+setTimeout(async function() { 
+  await queue.push('some value'); 
+  await queue.push('another value');
+
+  // 'another value'
+  tretas = queue.pop();
+  console.log('tretas')
+}, 3000);
+
+(async () => {
+    console.log('yay')
+    console.log(await cenas)
+    setTimeout(
+        async () => { console.log(await tretas) },
+        500
+    )
+})()
+
---
+++ publishSubscribe/src/stuff/Broker.ts
@@ -0,0 +1,49 @@
+import { Queue } from '../queue'
+import { Registry, DefaultDict } from '../utils'
+import { Publisher, SimpleSubscriber } from 'stuff';
+
+// Serves as a two-way ventilator
+export class Broker<T> {
+    // Registry key(Publisher | Subscriber) -> queue
+    private registry = new Registry<Queue.BlockingQueue<T>>()
+    // publisherKey -> arraySubscriberKeys
+    private observers = new DefaultDict<number, Array<number>>(() => new Array<number>())
+
+    constructor(public runTime: number) {}
+
+    addPublisher(obj: Publisher<T>) : number{
+        return this.registAndRun(obj)
+    }
+
+    addSubscriber(obj: SimpleSubscriber<T>, publisherId: number) :void {
+        const key = this.registAndRun(obj)
+        this.observers.get(publisherId).push(key)
+    }
+
+    registAndRun(obj: Publisher<T> | SimpleSubscriber<T>) :number{
+        let queue = new Queue.UnboundedQueue<T>()
+        const key = this.registry.register(queue);
+        obj.run(this.runTime, queue)
+        return key
+    }
+
+    async movesMessages(): Promise<void> {  
+        for await(let publisherKey of this.observers.keys()) {
+            let publisherQueue = this.registry.get(publisherKey)
+            const message = await publisherQueue.pop()
+
+            const subscribers = this.observers.get(publisherKey)
+            subscribers.forEach((subscriberKey) => {
+                let subscriberQueue = this.registry.get(subscriberKey)
+                subscriberQueue.push(message)
+            }) 
+        }
+    }
+
+    async run(): Promise<void> {
+        let start = Date.now()
+        while (start + this.runTime > Date.now()) {
+            await this.movesMessages()
+        }
+    }
+}
