fc1f383a611c60f87bb7635979b3761af06f6280
AndreFCruz <andrecruz97@gmail.com>
2019-06-03 18:25:22 +0100

    pubsub generics

--- src/pubsub/Publisher.java
+++ src/pubsub/Publisher.java
@@ -1,4 +1,13 @@
 package pubsub;
 
-public interface Publisher {
+public interface Publisher<Out> {
+
+    /**
+     * Method for this Source to generate a Message.
+     * May block!
+     *
+     * @return The generated Message
+     * @throws InterruptedException Thrown when interrupted
+     */
+    Out produceMessage() throws InterruptedException;
 }
--- src/pubsub/Subscriber.java
+++ src/pubsub/Subscriber.java
@@ -1,4 +1,14 @@
 package pubsub;
 
-public interface Subscriber {
+public interface Subscriber<In, Ret> {
+
+    /**
+     * Handle the given Message
+     * May block!
+     *
+     * @param message the message
+     * @return The handled message, if there's anything to return.
+     * @throws InterruptedException thrown when interrupted
+     */
+    Ret handleMessage(In message) throws InterruptedException;
 }
