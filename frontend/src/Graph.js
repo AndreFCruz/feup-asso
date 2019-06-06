4d2c92ac82cffdf231f71bc8296fa8c8943f487b
AndreFCruz <andrecruz97@gmail.com>
2019-06-06 11:58:34 +0100

    minor updates

--- frontend/src/Graph.js
+++ frontend/src/Graph.js
@@ -1,116 +1,17 @@
-// import React from 'react';
-
-// class Graph extends React.Component {
-//   constructor() {
-//     super();
-//     this.state = { someKey: 'someValue' };
-//   }
-
-//   render() {
-//     return <p>{this.state.someKey}</p>;
-//   }
-
-//   componentDidMount() {
-//     this.setState({ someKey: 'otherValue' });
-//   }
-// }
-
-// export default Graph;
-
 import React from 'react';
 import {
   GraphView, // required
-  Edge, // optional
+  // Edge, // optional
   // type IEdge, // optional
-  Node, // optional
+  // Node, // optional
   // type INode, // optional
   // type LayoutEngineType, // required to change the layoutEngineType, otherwise optional
-  BwdlTransformer, // optional, Example JSON transformer
-  GraphUtils // optional, useful utility functions
+  // BwdlTransformer, // optional, Example JSON transformer
+  // GraphUtils // optional, useful utility functions
 } from 'react-digraph';
 import './Graph.css';
-
-/// /// /// *** Graph Sample *** /// /// ///
-const sample = {
-  "nodes": [
-    {
-      "id": 1,
-      "title": "Node A",
-      "x": 258.3976135253906,
-      "y": 331.9783248901367,
-      "type": "empty"
-    },
-    {
-      "id": 2,
-      "title": "Node B",
-      "x": 593.9393920898438,
-      "y": 260.6060791015625,
-      "type": "empty"
-    },
-    {
-      "id": 3,
-      "title": "Node C",
-      "x": 237.5757598876953,
-      "y": 61.81818389892578,
-      "type": "custom"
-    },
-    {
-      "id": 4,
-      "title": "Node C",
-      "x": 600.5757598876953,
-      "y": 600.81818389892578,
-      "type": "custom"
-    }
-  ],
-  "edges": [
-    {
-      "source": 1,
-      "target": 2,
-      "type": "emptyEdge"
-    },
-    {
-      "source": 2,
-      "target": 4,
-      "type": "emptyEdge"
-    }
-  ]
-}
-/// /// /// *** Graph Sample *** /// /// ///
-
-
-const GraphConfig =  {
-  NodeTypes: {
-    empty: { // required to show empty nodes
-      typeText: "None",
-      shapeId: "#empty", // relates to the type property of a node
-      shape: (
-        <symbol viewBox="0 0 100 100" id="empty" key="0">
-          <circle cx="50" cy="50" r="45"></circle>
-        </symbol>
-      )
-    },
-    custom: { // required to show empty nodes
-      typeText: "Custom",
-      shapeId: "#custom", // relates to the type property of a node
-      shape: (
-        <symbol viewBox="0 0 50 25" id="custom" key="0">
-          <ellipse cx="50" cy="25" rx="50" ry="25"></ellipse>
-        </symbol>
-      )
-    }
-  },
-  NodeSubtypes: {},
-  EdgeTypes: {
-    emptyEdge: {  // required to show empty edges
-      shapeId: "#emptyEdge",
-      shape: (
-        <symbol viewBox="0 0 50 50" id="emptyEdge" key="0">
-          <circle cx="25" cy="25" r="8" fill="currentColor"> </circle>
-        </symbol>
-      )
-    }
-  }
-}
+import { GraphConfig as DEFAULT_GRAPH_CONFIG } from "./Graph.configs";
+import { sample1 as DEFAULT_GRAPH_SAMPLE } from "./Graph.sample";
 
 const NODE_KEY = "id"       // Allows D3 to correctly update DOM
 
@@ -120,21 +21,69 @@ export class Graph extends React.Component {
     super(props);
 
     this.state = {
-      graph: sample,
-      selected: {}
+      graph: DEFAULT_GRAPH_SAMPLE,
+      selected: {},
+      graphConfig: DEFAULT_GRAPH_CONFIG,
     }
   }
 
+  componentDidMount() {
+    // TODO:
+    // - fetch types of nodes for Sources/Handlers/Sinks
+    // - construct a GraphConfig object
+    // - update state with this.setState({...});
+  }
+
   /* Define custom graph editing methods here */
+
+  onSelectNode(event) {
+    console.log('** onSelectNode **');
+    console.log(event);
+  }
+
+  onCreateNode(event) {
+    console.log('** onCreateNode **');
+    console.log(event);
+  }
+
+  onUpdateNode(event) {
+    console.log('** onUpdateNode **');
+    console.log(event);
+  }
+
+  onDeleteNode(event) {
+    console.log('** onDeleteNode **');
+    console.log(event);
+  }
+
+  onSelectEdge(event) {
+    console.log('** onSelectEdge **');
+    console.log(event);
+  }
+
+  onCreateEdge(event) {
+    console.log('** onCreateEdge **');
+    console.log(event);
+  }
+
+  onSwapEdge(event) {
+    console.log('** onSwapEdge **');
+    console.log(event);
+  }
+
+  onDeleteEdge(event) {
+    console.log('** onDeleteEdge **');
+    console.log(event);
+  }
 
   render() {
     const nodes = this.state.graph.nodes;
     const edges = this.state.graph.edges;
     const selected = this.state.selected;
 
-    const NodeTypes = GraphConfig.NodeTypes;
-    const NodeSubtypes = GraphConfig.NodeSubtypes;
-    const EdgeTypes = GraphConfig.EdgeTypes;
+    const NodeTypes = this.state.graphConfig.NodeTypes;
+    const NodeSubtypes = this.state.graphConfig.NodeSubtypes;
+    const EdgeTypes = this.state.graphConfig.EdgeTypes;
 
     return (
       <div id='graph'>
